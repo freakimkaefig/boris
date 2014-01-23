b858b8eedbaeba25aa404a5f3e7d7f0b30064ffb
matTailor <Matthias.Schneider@stud.uni-regensburg.de>
2014-01-23 17:52:14 +0100

    checkboxes

--- src/cocktail_rating.php
+++ src/cocktail_rating.php
@@ -173,19 +173,19 @@
                <div class="row">
                     <div class="col-xs-6 col-md-6">
                             <label class="checkbox">
-                            <input type="checkbox" value="option1" id="amStrand">Am Strand</label>
+                                <input class="checkbox-input" type="checkbox" value="option1" id="amStrand">Am Strand</label>
                             <label class="checkbox">
-                            <input type="checkbox" value="option1" id="Firmenfeier">Firmenfeier</label>
+                                <input class="checkbox-input" type="checkbox" value="option2" id="Firmenfeier">Firmenfeier</label>
                             <label class="checkbox">
-                            <input type="checkbox" value="option1" id="Junggesellenabschied">Junggesellenabschied</label>
+                                <input class="checkbox-input" type="checkbox" value="option3" id="Junggesellenabschied">Junggesellenabschied</label>
                             <label class="checkbox">
-                            <input type="checkbox" value="option1" id="Hochzeit">Hochzeit</label>
+                                <input type="checkbox" value="option1" id="Hochzeit">Hochzeit</label>
                             <label class="checkbox">
-                            <input type="checkbox" value="option1" id="In der Cocktailbar">In der Cocktailbar</label>
+                                <input type="checkbox" value="option1" id="In der Cocktailbar">In der Cocktailbar</label>
                             <label class="checkbox">
-                            <input type="checkbox" value="option1" id="Nach dem Essen">Nach dem Essen</label>
+                                <input type="checkbox" value="option1" id="Nach dem Essen">Nach dem Essen</label>
                             <label class="checkbox">
-                            <input type="checkbox" value="option1" id="Daheim auf dem Sofa">Daheim auf dem Sofa</label>
+                                <input type="checkbox" value="option1" id="Daheim auf dem Sofa">Daheim auf dem Sofa</label>
                     </div>
                     <div class="col-xs-6 col-md-6">
                             <label class="checkbox">
@@ -211,7 +211,7 @@
                 <div class="col-xs-12 col-md-12">
 
                     <a href="#">
-                    <button id="Button1" type="button" class="btn btn-default pull-left" >
+                    <button id="button-send-rating" type="button" class="btn btn-default pull-left" >
                     <label style="font-size:20px; margin-top:5px;">Send Rating</label>
                     <span class="glyphicon glyphicon-chevron-right" style="font-size:20px;" ></span>
                     </button>
--- src/js/MainController.js
+++ src/js/MainController.js
@@ -23,6 +23,9 @@ Boris.MainController = function() {
     $ageInput = null,
     //gender radios group
     $radioGenderGroup = null,
+    $buttonSendRating = null,
+
+    $checkbox = null,
     //-----------Questionnaire_end--------------
 
 
@@ -44,7 +47,7 @@ Boris.MainController = function() {
         signView.init();
 
         $signInButton = $("#sign-in-button");
-        $signInButton.on("click", signIn);   
+        $signInButton.on("click", onSignIn);   
 
         //init radio groups for filtering       
         $radioTasteGroup = $(".taste");    
@@ -64,6 +67,11 @@ Boris.MainController = function() {
         $ageInput = $("#age-questionnaire");
         $radioGenderGroup = $(".genderRadios");
 
+        $buttonSendRating = $("#button-send-rating"); 
+        $buttonSendRating.on("click", onSendRating);
+
+        $checkbox = $(".checkbox-input");
+
         setRadioListenerBitterLikert();
         setRadioListenerSweetLikert();
         setRadioListenerSourLikert();
@@ -75,7 +83,7 @@ Boris.MainController = function() {
 
     },
 
-    signIn = function(event) {
+    onSignIn = function(event) {
         
         if(mainModel.getCorrectPassword() == signView.getInputValuePassword()) {
             if(mainModel.getUsernameForDrinkList() == signView.getInputValueUsername()) {
@@ -149,6 +157,25 @@ Boris.MainController = function() {
         $radioGenderGroup.change(function() {    
             mainModel.setGenderVal(event.target.value);
         });
+    },
+
+    handleCheckboxes = function() {
+        $checkbox.each(function(i, obj) {
+            
+        if (obj.checked) {
+              
+                mainModel.setActiveCheckboxes(obj.id);   
+            }
+        }); 
+    },
+
+    onSendRating = function(event) {
+        //hier dann ajax aufruf mit ganzen gettern und settern...
+        //event.preventDefault();
+        
+        handleCheckboxes();
+        mainModel.getActiveCheckboxes();
+        //hier noch die restlichen getter
     },
 	
 	onSearch = function(event, query) {		
