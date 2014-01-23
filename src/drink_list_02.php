d7e099f48c5dac4fa6bb90e4e18296c02a42cec5
freakimkaefig <lukas.lamm89@googlemail.com>
2014-01-23 10:50:21 +0100

    navbar markiert

--- src/drink_list_02.php
+++ src/drink_list_02.php
@@ -26,6 +26,8 @@
 </head>
 
 <body style="">
+
+<!-- ACTION BAR -->
 <div id="action-bar">
 	<div id="logo"><a href="#"><img src="img/logo_boris.png"></a></div>
     <div id="filter">
@@ -55,46 +57,7 @@
         <button id="search-submit" class="btn btn-default"><span class="glyphicon glyphicon-search"></span></button>
     </div>
 </div>
-<!--
-<div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-  <div class="container">
-    <div class="navbar-header"> <a class="navbar-brand" href="#"> <img src="img/logo_boris.png" class="img img-responsive" alt="Responsive image"> </a> </div>
-    <ul class="nav navbar-nav navbar-right">
-      <li class="dropdown"> <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-        <button type="button" class="btn btn-default"><span class="glyphicon glyphicon-filter"></span></button>-->
-        <!--Dropdown <b class="caret"></b>--> 
-        <!--</a>
-        <ul class="dropdown-menu">
-
-            <li class="dropdown-header">Taste</li>
-              <ul class="input-group" style="list-style-type:none">
-                  <li><input id="radio-taste-1" type="radio" name="groupTaste" value="Sweet" class="taste"/>Sweet</li>
-                  <li><input id="radio-taste-2" type="radio" name="groupTaste" value="Fruity" class="taste"/>Fruity</li>
-                  <li><input id="radio-taste-3" type="radio" name="groupTaste" value="Sour" class="taste"/>Sour</li>
-                  <li><input id="radio-taste-4" type="radio" name="groupTaste" value="Bitter" class="taste"/>Bitter</li>
-              </ul>
-
-            <li class="dropdown-header">Alcohol Strength</li>
-              <ul class="input-group" style="list-style-type:none">
-                  <li><input id="radio-alc-1" type="radio" name="groupAlc" value="Alc-Free" class="alc"/>Alc-Free</li>
-                  <li><input id="radio-alc-2" type="radio" name="groupAlc" value="Weak" class="alc"/>Weak</li>
-                  <li><input id="radio-alc-3" type="radio" name="groupAlc" value="Middle" class="alc"/>Middle</li>
-                  <li><input id="radio-alc-4" type="radio" name="groupAlc" value="Strong" class="alc"/>Strong</li>
-              </ul>
-
-        </ul>
-      </li>
-      <form class="navbar-form navbar-left" role="search" action="javascript:void(0);">
-        <div class="form-group">
-          <input id="search-input" type="text" class="form-control" placeholder="Search" />
-        </div>
-        <button id="search-submit" type="" class="btn btn-default"><span class="glyphicon glyphicon-search"></span></button>
-      </form>
-      </li>
-    </ul>
-  </div>
-</div>
--->
+<!-- END ACTION BAR -->
 
 <!-- Main jumbotron for a primary marketing message or call to action -->
 <div class="container drink-list-container"> 
@@ -109,7 +72,7 @@
 ?>
   <?php foreach ($cocktails as $cocktail_id  => $cocktail): ?>
   <a href="#" class="no-btn" role="button">
-  <div class="row row-<?php print $cocktail_id; ?><?php if($cocktail_id==1) print " first"?><?php if($cocktail==end($cocktails)) print " last" ?> col-xs-12 col-md-6 col-lg-4 inline-block">
+  <div class="row row-<?php print $cocktail_id; ?><?php if($cocktail_id==1) print " first"?><?php if($cocktail==end($cocktails)) print " last" ?> col-xs-12 col-md-6 col-lg-6 inline-block">
     <div class="col-xs-4"> <img src="img/drink_example.jpg" class="img img-responsive" alt="Responsive image" /> </div>
     <div class="col-xs-4">
       <label><?php print $cocktail->name; ?></label>
