22dba85573218be3b312a3192d252f5e6ea74757
freakimkaefig <lukas.lamm89@googlemail.com>
2014-01-23 13:31:28 +0100

    search

--- src/js/SearchView.js
+++ src/js/SearchView.js
@@ -1,97 +1,77 @@
-Boris.SearchView = function() {
+Boris.SearchView = function () {
     var that = {},
 		mainController,
 		searchInput,
 		searchSubmit,
 
-    init = function() {
+    init = function () {
         console.log("search view init");
-		
-		mainController = Boris.MainController();
-		$(mainController).on('searchresult', onSearchResult);
-		
+
+        mainController = Boris.MainController();
+        $(mainController).on('searchresult', onSearchResult);
+
         $searchInput = $("#search-input");
         $searchSubmit = $("#search-submit");
-		
-		$searchSubmit.on('click', onSearchSubmitClick);
+
+        $searchSubmit.on('click', onSearchSubmitClick);
     },
-	
-	getSearchInput = function() {
-		return $searchInput.val();
+
+	getSearchInput = function () {
+	    return $searchInput.val();
 	},
-	
-	onSearchSubmitClick = function() {
-		$(that).trigger('search', getSearchInput());
+
+	onSearchSubmitClick = function () {
+	    $(that).trigger('search', getSearchInput());
 	},
-	
-	onSearchResult = function(result) {
-		console.log("searchResult", result);
-		
-		if(result.name && !result.ingredient) {
-			//	Name & !Zutat
-			console.log("Name & !Zutat");
-			
-		} else if(!result.name && result.ingredient) {
-			//	!Name & Zutat
-			console.log("!Name & Zutat");
-			//console.log(result.ingredient);
-			var cocktailIdsToHide = new Array();
-			var tempArr = $.map(result.ingredient.data, function(value, index) {
-				//console.log("map", value, index);
-				return [parseInt(index)];
-			});
-			console.log("cocktailIds", result.numCocktails);
-			console.log("tempArr", tempArr);
-			
-			//Prüfen, welche ID nicht in tempArr enthalten!!!
-			/*
-			for (var i=1; i<=result.numCocktails; i++) {
-				for (var j=0; j<tempArr.length; j++) {
-					if(i != tempArr[j]) {
-						console.log("hide", i);
-					}
-				}
-			}
-			*/
-			thirdArray = new Array();
-			
-			for(var i = 0; i<result.numCocktails.length; i++) {
-				for (var j = 0; i<tempArr.length; j++) {
-					if(result.numCocktails[i] != tempArr[j]) {
-						if(thirdArray == null) {
-							thirdArray.push(result.numCocktails[i])
-						} else {
-							for(var x= 0; x<thirdArray.length; x++) {
-								if(result.numCocktails[i] != thirdArray[x])
-									thirdArray.push(result.numCocktails[i])
-								}
-							}
-						}
-					}
-				}
-			console.log(thirdArray);
-			
-			//console.log("hide", cocktailIdsToHide);
-			//hideCocktailsById();
-			
-		} else if(!result.name && !result.ingredient) {
-			//	!Name & !Zutat
-			console.log("!Name & !Zutat");
-			
-		} else if(result.name && result.ingredient) {
-			//	Name & Zutat
-			console.log("Name & Zutat");
-			
-		}
+
+	onSearchResult = function (result) {
+	    console.log("searchResult", result);
+
+	    if (result.name && !result.ingredient) {
+	        //	Name & !Zutat
+	        console.log("Name & !Zutat");
+
+	    } else if (!result.name && result.ingredient) {
+	        //	!Name & Zutat
+	        console.log("!Name & Zutat");
+	        //console.log(result.ingredient);
+	        var cocktailIdsToHide = new Array();
+	        var tempArr = $.map(result.ingredient.data, function (value, index) {
+	            //console.log("map", value, index);
+	            return [parseInt(index)];
+	        });
+	        console.log("cocktailIds", result.numCocktails);
+	        console.log("tempArr", tempArr);
+
+	        //Prüfen, welche ID nicht in tempArr enthalten!!!
+
+	        for (var i = 1; i <= result.numCocktails.length; i++) {
+	            if (tempArr.indexOf(i) == -1) {
+	                console.log("hide", i);
+	            }
+	        }
+
+	        //console.log("hide", cocktailIdsToHide);
+	        //hideCocktailsById();
+
+	    } else if (!result.name && !result.ingredient) {
+	        //	!Name & !Zutat
+	        console.log("!Name & !Zutat");
+
+	    } else if (result.name && result.ingredient) {
+	        //	Name & Zutat
+	        console.log("Name & Zutat");
+
+	    }
 	},
-	
-	hideCocktailsById = function(arrayIds) {
-		
+
+	hideCocktailsById = function (arrayIds) {
+
 	};
 
 
     that.init = init;
-	that.onSearchResult = onSearchResult;
+    that.onSearchResult = onSearchResult;
 
 
     return that;
